be61d08e23a183e2f158a40db606de6fb24232ff
PatadeSujal <patadesujal@gmail.com>
2025-10-04 22:34:34 +0530

    post button resolve

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -4,6 +4,7 @@ import { Search, Menu, X } from "lucide-react";
 import Link from "next/link";
 import { useDispatch, useSelector } from "react-redux";
 import { addItem, clearItems } from "../store/dataSlice";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -52,8 +53,8 @@ const Navbar = () => {
   }, [query]);
 
   return (
-    <div className="container w-[80%] flex justify-center mx-auto mt-2">
-      <nav className="bg-white shadow-lg sticky top-0 z-50 w-full rounded-2xl">
+    <div className="container w-[80%] flex justify-center mx-auto mt-2 ">
+      <nav className="bg-white shadow-lg  z-50 w-full rounded-2xl static top-0">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-evenly items-center h-16">
             {/* Logo */}
@@ -97,25 +98,26 @@ const Navbar = () => {
                       {item.hashtags.split(" ").map((hashtag) => {
                         return (
                           <span className="px-2 sm:px-3 py-1 text-xs font-medium text-blue-700 bg-blue-50 border border-blue-200 rounded-full">
-                            {hashtag}
+                          {hashtag}
                           </span>
                         );
-                      })}
-                    </li>
-                  ))}
-                </ul>
-              )}
-            </div>
-            <Link href="/post">
-            <button
-              type="button"
-              class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
-              >
-              Post Yours
-            </button>
-              </Link>
-            {/* Mobile menu button */}
-            <div className="md:hidden">
+                        })}
+                      </li>
+                      ))}
+                    </ul>
+                    )}
+                  </div>
+                  <Link href="/post">
+                    <button
+                    type="button"
+                    className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-4 py-2 text-center me-2 mb-2"
+                    >
+                    <span className="md:hidden font-extrabold text-2xl">+</span>
+                    <span className="hidden md:inline">Post Yours</span>
+                    </button>
+                  </Link>
+                  {/* Mobile menu button */}
+            {/* <div className="md:hidden">
               <button
                 onClick={toggleMenu}
                 className="inline-flex items-center justify-center p-2 rounded-md text-gray-700 hover:text-blue-600 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-500"
@@ -126,7 +128,7 @@ const Navbar = () => {
                   <Menu className="block h-6 w-6" />
                 )}
               </button>
-            </div>
+            </div> */}
           </div>
         </div>
 
